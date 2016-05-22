c61db2b4ba234f8b8c6d9f08f490ed4006fac7c1
Travis Shafer <travisshafer@Traviss-MacBook-Pro.local>
2016-05-21 18:48:22 -0700

    added working twitter api calls and individual user oauth

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
-var app = angular.module('myApp', []);
-app.controller('controller', function($scope, $http, dataService) {
+  var app = angular.module('cntxt', ['ngSanitize','cntxt.services']);
+app.controller('controller', function($scope, $http, $q, twitterService) {
 
     $scope.url = null
 
@@ -13,6 +13,7 @@ app.controller('controller', function($scope, $http, dataService) {
 
     $scope.getAllData = function(){
     	getRedditData();
+      searchTweets();
     	//drawRedditChart();
     	//getTwitterData();
 
@@ -123,112 +124,66 @@ function getTopComment(subreddit, id, score){
 	}//end getTopComment
 
 
-	//draw reddit chart
-	// function drawRedditChart() {
-	// 	var chart_data = [];
+//begin twitter stuff
+$scope.tweets = []; //array of tweets
 
-	// 	for(var i = 0; i < $scope.redditResponse['data']['children']length; i++){
-	// 		var listing = $scope.redditResponse.data.children[i];
-	// 		//console.log(listing.data.num_comments, listing.data.subreddit);
-	// 		chart_data.push({comments: listing.data.num_comments, subreddit: listing.data.subreddit});
-	// 	}
+twitterService.initialize();
 
-		// var sorted_chart_data = chart_data.slice(0);
+function searchTweets(){
+    twitterService.getMatchingTweets($scope.url).then(function(data){
+        console.log(data);
+        $scope.tweets = data.statuses;
+        //$scope.tweets = $scope.tweets.concat(data);
+    }, function(){
+        $scope.rateLimitError = true;
+    })
+}
 
-		// sorted_chart_data.sort(function(a,b){return b.comments-a.comments})
+//using the OAuth authorization result get the latest 20 tweets from twitter for the user
+$scope.refreshTimeline = function(maxId) {
+    twitterService.getLatestTweets(maxId).then(function(data) {
+        $scope.tweets = $scope.tweets.concat(data);
+    }, function() {
+        $scope.rateLimitError = true;
+    });
+}
 
-		// var arrayToDataTable_data = [
-		// ['Subreddit', 'Number of Comments']
-		// ];
+//when the user clicks the connect twitter button, the popup authorization window opens
+$scope.connectButton = function() {
+    twitterService.connectTwitter().then(function() {
+        if (twitterService.isReady()) {
+            //if the authorization is successful, hide the connect button and display the tweets
+            $('#connectButton').fadeOut(function() {
+                $('#getTimelineButton, #signOut').fadeIn();
+                $scope.connectedTwitter = true;
+            });
+        } else {
 
-		// for(var j = 0; j<sorted_chart_data.length; j++){
-		// 	arrayToDataTable_data.push(["/r/"+sorted_chart_data[j].subreddit, sorted_chart_data[j].comments]);
-		// }
+        }
+    });
+}
 
-		// var data = google.visualization.arrayToDataTable(arrayToDataTable_data);
+//sign out clears the OAuth cache, the user will have to reauthenticate when returning
+$scope.signOut = function() {
+    twitterService.clearCache();
+    $scope.tweets.length = 0;
+    $('#getTimelineButton, #signOut').fadeOut(function() {
+        $('#connectButton').fadeIn();
+        $scope.$apply(function() {
+            $scope.connectedTwitter = false
+        })
+    });
+}
 
-		// //SAVED TO SHOW SYNTAX OF CHART BUIDLING 
-  //     // var data = google.visualization.arrayToDataTable([
-  //     //   ['Subreddit', 'Number of Comments'],
-  //     //   ["/r/"+sorted_chart_data[0].subreddit, sorted_chart_data[0].comments],
-  //     //   ["/r/"+sorted_chart_data[1].subreddit, sorted_chart_data[1].comments],
-  //     //   ["/r/"+sorted_chart_data[2].subreddit, sorted_chart_data[2].comments],
-  //     //   ["/r/"+sorted_chart_data[3].subreddit, sorted_chart_data[3].comments],
-  //     //   ["/r/"+sorted_chart_data[4].subreddit, sorted_chart_data[4].comments],
-  //     //   ["/r/"+sorted_chart_data[5].subreddit, sorted_chart_data[5].comments]
-  //     // ]);
-
-  //     var options = {
-  //     	legend: 'none',
-  //       title: 'Active Conversations on Reddit',
-  //       chartArea: {width: '50%'},
-  //       hAxis: {
-  //         title: 'Number of Comments',
-  //         minValue: 0
-  //       },
-  //       vAxis: {
-
-  //       }
-  //     };
-
-  //     var chart = new google.visualization.BarChart(document.getElementById('reddit-comments-chart'));
-
-  //     chart.draw(data, options);
-  //   }
-
-
-
-
-
-
-// // Simple GET request example:
-// $http({
-//   method: 'GET',
-//   url: 'https://' + bearer_token + '@api.twitter.com/1.1/search/tweets.json?q=@travisshafer',
-//   headers: {
-//   		'Authorization': 'Bearer ' + bearer_token,
-//   		'Allow-Enconding': 'gzip'
-//   }
-// }).then(function successCallback(response) {
-// 	console.log("nice!");
-//   }, function errorCallback(response) {
-//    console.log("crap!");
-//   });
-
-// }
-
-// GET /1.1/search/tweets.json?count=100&result_type=recent&q=%24MMM HTTP/1.1
-// User-Agent: curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8x zlib/1.2.5
-// Host: api.twitter.com
-// Accept: /
-// Authorization: Bearer xxxxxxxxx
-
-
-
-
-
+//if the user is a returning user, hide the sign in button and display the tweets
+if (twitterService.isReady()) {
+    $('#connectButton').hide();
+    $('#getTimelineButton, #signOut').show();
+    $scope.connectedTwitter = true;
+    //$scope.refreshTimeline();
+}
 
 });
 
-app.service('dataService', function($http) {
-delete $http.defaults.headers.common['X-Requested-With'];
-this.getTwitterData = function() {
-    // $http() returns a $promise that we can add handlers with .then()
-    return $http({
-        method: 'GET',
-        url: 'https://api/twitter.com/1.1/search/tweets.json',
-        params: {'q': 'hello'},
-        headers: {'Authorization': 'Bearer AAAAA...(use code reference here)'}
-
-     });
- }
-});
-
-// app.controller('AngularJSCtrl', function($scope, dataService) {
-//     $scope.data = null;
-//     dataService.getTwitterData().then(function(dataResponse) {
-//         $scope.data = dataResponse;
-//     });
-// };
 
 
